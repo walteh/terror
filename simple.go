911723308df702865c7e9a926f15d68b59b5f854
Walter Scott <w@lter.ca>
2023-12-16 11:05:30 -0600

    add mismatch

--- simple.go
+++ simple.go
@@ -2,6 +2,8 @@ package terrors
 
 import (
 	"fmt"
+
+	"github.com/rs/zerolog"
 )
 
 // New returns an error that formats as the given text.
@@ -19,4 +21,10 @@ func New(text string) *wrapError {
 // implements Formatter to show this information when printed with details.
 func Errorf(format string, a ...any) *wrapError {
 	return WrapWithCaller(nil, fmt.Sprintf(format, a...), 1)
+}
+
+func Mismatch[T any](expected, actual T) *wrapError {
+	return WrapWithCaller(nil, "mismatch", 1).Event(func(e *zerolog.Event) *zerolog.Event {
+		return e.Interface("expected", expected).Interface("actual", actual)
+	}).(*wrapError)
 }
