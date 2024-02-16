3dda85fa8bf8dff139e11e3425cfa5c817b32393
Walter Scott <w@lter.ca>
2024-02-16 06:18:17 -0600

    adjust recovery logic

--- into.go
+++ into.go
@@ -1,6 +1,9 @@
 package terrors
 
-import "errors"
+import (
+	"errors"
+	"slices"
+)
 
 // Into finds the first error in err's chain that matches target type T, and if so, returns it.
 //
@@ -11,8 +14,11 @@ func Into[T error](err error) (val T, ok bool) {
 }
 
 func IsRecoverable(err error) (bool, *Recovery) {
-	// look for any recoverable error in the chain
 	chain := GetChain(err)
+
+	// we want to get the deepest recoverable error in the chain
+	slices.Reverse(chain)
+
 	for _, e := range chain {
 		if werr, ok := e.(*wrapError); ok {
 			if werr.recovery != nil {
