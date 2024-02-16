cfaae96782a689864292155bc7488b082558c3af
Walter Scott <w@lter.ca>
2024-02-16 12:19:31 -0600

    detailed self only message

--- wrap.go
+++ wrap.go
@@ -113,7 +113,7 @@ func FullChainFormatter(kid error) string {
 		wrk += arrow + " "
 		switch v := err.(type) {
 		case *wrapError:
-			wrk += v.DetailedSelf()
+			wrk += v.DetailedSelfOnlyMessage()
 		default:
 			wrk += fmt.Sprintf("%s\n\n", v.Error())
 		}
@@ -158,6 +158,18 @@ func (e *wrapError) Self() string {
 }
 
 func (e *wrapError) DetailedSelf() string {
+	self := e.Self()
+
+	dets := e.Detail()
+
+	if dets != "" {
+		self += fmt.Sprintf("\n\n%s\n\n", dets)
+	}
+
+	return self
+}
+
+func (e *wrapError) DetailedSelfOnlyMessage() string {
 	self := e.Self()
 
 	dets := e.Detail()
